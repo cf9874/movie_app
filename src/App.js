7f66dc10c60cbf992ecf20dab7ff1e84c4807c68
gamcha12 <68059873+gamcha12@users.noreply.github.com>
2021-02-05 19:04:48 +0900

    load movies , axios, async & await

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 //  import './App.css';
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movie: [],
   };
+
+  getMoivies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    // axios.get이 항상 빠른 것은 아니기 때문에 CDM함수가 끝날 때 까지
+    // JS에게 기다리라는 요청을 해야한다. >> async , await
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 2000);
+    this.getMoivies();
   }
   render() {
     const { isLoading } = this.state;
